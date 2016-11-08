5df47475d7b17d1c95839032cfa3f3f7996bc508
Ross Hadden <rosshadden@gmail.com>
2016-11-08 10:04:46 -0500

    Fixed building when no local-conf.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-moonc -t dist vendor lib src {conf,local-conf,main}.moon
+moonc -t dist vendor lib src {conf,main}.moon
+if [ -f local-conf.moon ]; then moonc -t dist local-conf.moon; fi
 
 for map in assets/maps/*.tmx; do
 	output=$(echo "$map" | sed 's;\.tmx$;.lua;')
